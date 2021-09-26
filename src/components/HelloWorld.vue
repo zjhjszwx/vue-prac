c2bcce8bfc9bf877073012db897abfae42e9c1ce
zhangwenxin <17830723072@dingtalk.com>
2021-09-26 17:35:18 +0800

    temp

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,24 @@
 <template lang="">
   <div>
-    213
+    {{ message }}
   </div>
+  <div v-once>{{ message }}</div>
+  <div v-html="msgHtml"></div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      message: "欢迎来到Vue的世界",
+      msgHtml: "<p style='color: red'>欢迎来到红色Vue的世界</p>",
+    };
+  },
+  created() {
+    this.message = "啦啦啦啦啦啦";
+  },
+  mounted() {
+    this.message = "略略略略略";
+  },
+};
 </script>
 <style lang=""></style>
