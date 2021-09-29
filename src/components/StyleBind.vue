941c9ebaaa1b6ef49c10835afc5819098347b6d5
zhangwenxin <17830723072@dingtalk.com>
2021-09-29 16:35:56 +0800

    模板语法

--- src/components/StyleBind.vue
+++ src/components/StyleBind.vue
@@ -1,13 +1,47 @@
 <template lang="">
-  <div>
-    
+  <div class="static" :class="classObject">
+    11
   </div>
+  <div @click="counter += 1">* {{ counter }}</div>
+  <div @click="add">+</div>
+  <input type="text" v-model="message" placeholder="edit me" />
+  <p>Message is {{ message }}</p>
+  <select v-model="selected">
+    <option v-for="option in options" :value="option.value" :key="option.value">
+      {{ option.text }}
+    </option>
+  </select>
+  <span>Selected: {{ selected }}</span>
 </template>
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      isActive: true,
+      hasError: true,
+      classObject: {
+        test: true,
+        "text-danger": false,
+      },
+      counter: 0,
+      message: "message",
+      selected: "A",
+      options: [
+        { text: "One", value: "A" },
+        { text: "Two", value: "B" },
+        { text: "Three", value: "C" },
+      ],
+    };
+  },
+  methods: {
+    add: function(event) {
+      this.counter += 1;
+    },
+  },
+};
 </script>
-<style lang="">
-  
+<style>
+.static {
+  color: red;
+}
 </style>
