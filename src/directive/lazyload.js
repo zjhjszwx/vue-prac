df14bc5a20a947b5fc792ed4e38cee38de38068e
zjhjszwx <526084500@qq.com>
2022-03-28 14:20:41 +0800

    lazyload

--- src/directive/lazyload.js
+++ src/directive/lazyload.js
@@ -7,8 +7,10 @@ const lazyload = {
         lazyload.init(el, binding.value, defaultSrc)
       },
       inserted(el) {
-        if (IntersectionObserver) {
+        if (!IntersectionObserver) {
           lazyload.observe(el)
+        } else {
+          lazyload.listenerScroll(el)
         }
       }
     })
@@ -20,18 +22,60 @@ const lazyload = {
   observe(el) {
     let io = new IntersectionObserver((entries) => {
       if (entries[0].isIntersecting) {
-        let src = el.dataset.src
-        if (src) {
-          // 演示效果
-          setTimeout(() => {
-            el.src = src
-            el.removeAttribute('data-src')
-          }, 1000)
-        }
+        lazyload.changeSrc(el)
       }
     })
     io.observe(el)
-  }
+  },
+  listenerScroll(el){
+    const handler = lazyload.throttle(lazyload.load, 3000)
+    window.addEventListener('scroll', () => {
+      // lazyload.load(el)
+      handler(el)
+    })
+  },
+  load(el) {
+    let docHeight = document.documentElement.clientHeight
+    let eTop = el.getBoundingClientRect().top
+    let eBottom = el.getBoundingClientRect().bottom
+    console.log(el.getBoundingClientRect())
+    if(eTop - docHeight < 0 && eBottom > 0) {
+      lazyload.changeSrc(el)
+    }
+  },
+  changeSrc(el) {
+    let src = el.dataset.src
+    if (src) {
+      // 演示效果
+      setTimeout(() => {
+        el.src = src
+        el.removeAttribute('data-src')
+      }, 1000)
+    }
+  },
+  throttle(fn, delay) {
+    let timer
+    let prevTime
+    return function (...args) {
+      const currTime = Date.now()
+      const context = this
+      if (!prevTime) prevTime = currTime
+      clearTimeout(timer)
+
+      if (currTime - prevTime > delay) {
+        prevTime = currTime
+        fn.apply(context, args)
+        clearTimeout(timer)
+        return
+      }
+
+      timer = setTimeout(function () {
+        prevTime = Date.now()
+        timer = null
+        fn.apply(context, args)
+      }, delay)
+    }
+  },
 }
 
 export default lazyload
